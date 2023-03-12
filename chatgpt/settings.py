57f4dc4af47c43876b1b95e53ef67decea653f07
alaminmagaga <alaminmusamagaga@gmail.com>
2023-03-11 19:21:38 -0800

    update

--- chatgpt/settings.py
+++ chatgpt/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$1)t9ph-*ar9&8mfhhzgt=-02*s_zg_qval2!0p+pilil&#n+t"
+SECRET_KEY = "Your openAI api key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
